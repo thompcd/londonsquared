11a0c6baecf3cd0fb7446e136f5995d972ed954f
Corey Thompson <corey.dwayne.thompson@gmail.com>
2025-04-07 11:24:49 +0000

    able to load. Overfetching london data

--- src/ls-layout.js
+++ src/ls-layout.js
@@ -3,7 +3,7 @@ export default [
 	{"LSAbbreviation":"twnw","x":"2","y":"2","irregular":true,"code":"74107","name":"Town West"},
 	{"LSAbbreviation":"trky","x":"2","y":"3","irregular":false,"code":"74132","name":"Tulsa Hills"},
 	{"LSAbbreviation":"jnks","x":"2","y":"4","irregular":true,"code":"74037","name":"Jenks"},
-	{"LSAbbreviation":"mwhk","x":"3","y":"0","irregular":false,"code":"74106","name":"Mohawk Park"},
+	{"LSAbbreviation":"mwhk","x":"3","y":"0","irregular":false,"code":"74115","name":"Mohawk Park"},
 	{"LSAbbreviation":"dwnt","x":"3","y":"1","irregular":true,"code":"74103","name":"Downtown"},
 	{"LSAbbreviation":"utcs","x":"3","y":"2","irregular":true,"code":"74152","name":"Utica Square"},
 	{"LSAbbreviation":"brks","x":"3","y":"3","irregular":false,"code":"74105","name":"Brookside"},
